5d4e6d6e53a490fe3d4f203bbdc0563a0e5ba0df
gabrielibagon <gabriel.ibagon@gmail.com>
2021-01-04 09:03:16 -0500

    speed benchmarking code

---
+++ benchmark.py
@@ -0,0 +1,108 @@
+import argparse
+import torch
+import yaml
+from tqdm import tqdm
+
+from models.experimental import attempt_load
+from utils.datasets import create_dataloader
+from utils.general import coco80_to_coco91_class, check_dataset, check_file, check_img_size, box_iou, \
+    non_max_suppression, scale_coords, xyxy2xywh, xywh2xyxy, set_logging, increment_path
+from utils.loss import compute_loss
+from utils.torch_utils import select_device, time_synchronized
+
+
+
+def benchmark(data,
+             weights=None,
+             batch_size=32,
+             imgsz=640):  # number of logged images
+
+        # Initialize/load model and set device
+        device = select_device(opt.device, batch_size=batch_size)
+
+        # Load model
+        model = attempt_load(weights, map_location=device)  # load FP32 model
+        imgsz = check_img_size(imgsz, s=model.stride.max())  # check img_size
+
+        # Half
+        half = device.type != 'cpu'  # half precision only supported on CUDA
+        if half:
+            model.half()
+
+        # Configure
+        model.eval()
+        with open(data) as f:
+            data = yaml.load(f, Loader=yaml.FullLoader)  # model dict
+        check_dataset(data)  # check
+
+        # Dataloader
+        img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
+        _ = model(img.half() if half else img) if device.type != 'cpu' else None  # run once
+        path = data['test'] if opt.task == 'test' else data['val']  # path to val/test images
+        dataloader = create_dataloader(path, imgsz, batch_size, model.stride.max(), opt, pad=0.5, rect=True)[0]
+
+        seen = 0
+        s = f'Computing benchmarks for batch size: {batch_size}'
+        p, r, f1, mp, mr, map50, map, t0, t1 = 0., 0., 0., 0., 0., 0., 0., 0., 0.
+        loss = torch.zeros(3, device=device)
+        for batch_i, (img, targets, paths, shapes) in enumerate(tqdm(dataloader, desc=s)):
+            img = img.to(device, non_blocking=True)
+            img = img.half() if half else img.float()  # uint8 to fp16/32
+            img /= 255.0  # 0 - 255 to 0.0 - 1.0
+            targets = targets.to(device)
+            nb, _, height, width = img.shape  # batch size, channels, height, width
+
+            with torch.no_grad():
+                # Run model
+                t = time_synchronized()
+                inf_out, train_out = model(img, augment=False)  # inference and training outputs
+                t0 += time_synchronized() - t
+
+                # Compute loss
+                loss += compute_loss([x.float() for x in train_out], targets, model)[1][:3]  # box, obj, cls
+
+                # Run NMS
+                targets[:, 2:] *= torch.Tensor([width, height, width, height]).to(device)  # to pixels
+                t = time_synchronized()
+                output = non_max_suppression(inf_out)
+                t1 += time_synchronized() - t
+            seen+=1
+
+        # Print speeds
+        t = tuple(x / seen * 1E3 for x in (t0, t1, t0 + t1)) + (imgsz, imgsz, batch_size)  # tuple
+        print('Speed: %.1f/%.1f/%.1f ms inference/NMS/total per %gx%g image at batch-size %g' % t)
+
+        return t
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='test.py')
+    parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
+    parser.add_argument('--data', type=str, default='data/coco128.yaml', help='*.data path')
+    parser.add_argument('--batch-size', type=int, default=32, help='size of each image batch')
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.001, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.6, help='IOU threshold for NMS')
+    parser.add_argument('--task', default='val', help="'val', 'test', 'study'")
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--single-cls', action='store_true', help='treat as single-class dataset')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--verbose', action='store_true', help='report mAP by class')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-hybrid', action='store_true', help='save label+prediction hybrid results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--save-json', action='store_true', help='save a cocoapi-compatible JSON results file')
+    parser.add_argument('--project', default='runs/test', help='save to project/name')
+    parser.add_argument('--name', default='exp', help='save to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    opt = parser.parse_args()
+    opt.save_json |= opt.data.endswith('coco.yaml')
+    opt.data = check_file(opt.data)  # check file
+    print(opt)
+
+    batch_sizes = [1, 2, 4, 8, 16, 32, 64, 128]
+    for batch_size in batch_sizes:  # img-size
+        r, _, t = benchmark(data=opt.data,
+                            weights=opt.weights,
+                            batch_size=batch_size)
+# python benchmark.py --source /Users/gabrielibagon/repos/ProductTracker/data/videos/0000000000000158410.mp4 --weights /Users/gabrielibagon/repos/ProductTracker/sagemaker/container/yolov5/weights/yolov5s.pt
